30bcba26200b16c0834a3ed63a57eef4dd099a2d
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-01-16 22:11:40 -0300

    code clean

--- particles.js
+++ particles.js
@@ -1,29 +1,14 @@
-;(async () => {
-  await loadStarsPreset(tsParticles) // this is required only if you are not using the bundle script
-
-  await tsParticles.load('tsparticles', {
-    preset: 'stars'
-  })
-})()
-
 tsParticles.load('tsparticles', {
   particles: {
     shape: {
       type: 'circle' // starting from v2, this require the square shape script
     },
-    links: {
-      enable: false,
-      opacity: 0.2
+    size: {
+      value: { min: 1, max: 4 }
     },
     move: {
       enable: true,
       speed: 0.1
-    },
-    opacity: {
-      value: 0.2
-    },
-    size: {
-      value: { min: 1, max: 4 }
     }
   },
   preset: 'stars'
