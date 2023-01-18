aa297dfe21324f4a075f4c7ec7e6a9365421cb9e
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-01-17 21:58:33 -0300

    days and habits added using forms

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-document.querySelector('.teste').classList.toggle('click')
+function newDay(day) {
+  return `
+  <div class="day">
+  <h3>${day}/01</h3>
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  </div>
+  `
+}
+
+document.querySelector('#days').innerHTML =
+  newDay(17) +
+  newDay(18) +
+  newDay(19) +
+  newDay(20) +
+  newDay(21) +
+  newDay(22) +
+  newDay(23)
